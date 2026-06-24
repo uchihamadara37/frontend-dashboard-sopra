3dd62a077feec416d9224363cd2c50cacd86d7ce
andre <andreamusholin@gmail.com>
2026-06-24 08:09:06 +0700

    mengedit config next

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*', // Jika frontend memanggil rute yang berawalan /api/...
+        destination: 'http://163.61.58.96:5000/api/:path*', // ...Vercel yang akan meneruskannya ke VPS ini
+      },
+    ]
+  },
 };
 
 export default nextConfig;
